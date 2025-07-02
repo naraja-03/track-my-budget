3f6f076ffb767f502ab498ceac8df345a265afb6
Naraja <santhanarajadev3@gmail.com>
2025-07-02 09:56:07 +0530

    addgoogle verify

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="google-site-verification" content="p4V8I5x5gUgu3vS7Axo9igHca40E6-c55HPxhe_wUaQ" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
